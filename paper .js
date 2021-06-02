b2b7d7ac9705ab07384e808709d1c10ad0455adc
janaranjanibharani <75161006+janaranjanibharani@users.noreply.github.com>
2021-06-02 17:09:02 +0530

    Add files via upload

---
+++ paper .js
@@ -0,0 +1,18 @@
+class Paper{
+    constructor(x,y,r){
+        var option={
+         isStatic:false,
+         restitution:0.3,
+         friction:0.5,
+         density:0.8,   
+        }
+  this .body=Bodies.circle (x,y,r,option)
+  this.image =loadImage("paper.png");
+  this. r =r
+World .add(world,this.body)
+    }
+    display (){
+    imageMode(CENTER);   
+    image(this.image,this. body.position.x,this .body.position.y,this .r,this .r) 
+    }
+}
